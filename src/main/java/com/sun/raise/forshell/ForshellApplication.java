a1a42afa1aad934781689e239ec4b79a77cf1ac1
chenxiaolong <finding0@sina.cn>
2019-02-22 09:31:03 +0800

    13

---
+++ src/main/java/com/sun/raise/forshell/ForshellApplication.java
@@ -0,0 +1,13 @@
+package com.sun.raise.forshell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForshellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForshellApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sun/raise/forshell/ForshellApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sun.raise.forshell;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ForshellApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
